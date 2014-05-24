1d3a67147009971b055b5499219d98bfe8554d7b
Steffen Tröster <stetro.freaky@googlemail.com>
2014-05-24 18:28:04 +0200

    adding some routes

--- server/index.php
+++ server/index.php
@@ -1,6 +1,20 @@
 <?php
 require 'vendor/autoload.php';
 $app = new \Slim\Slim();
+
+header('Access-Control-Allow-Origin: *'); 
+header('Access-Control-Allow-Methods: POST, GET');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Headers: X-Requested-With');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Headers: Authorization');
+
+$host = 'localhost';
+$dbname = 'stetro_pickmeapp';
+$user = 'stetro';
+$pass = '';	
+$DBH= new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+
 function sendGoogleCloudMessage( $data, $ids )
 {
 	$url = 'https://android.googleapis.com/gcm/send';
@@ -10,9 +24,9 @@ function sendGoogleCloudMessage( $data, $ids )
 	        'data'              => $data,
 	    );
 	$headers = array( 
-       'Authorization: key=' . $apiKey,
-        'Content-Type: application/json'
-    );
+		'Authorization: key=' . $apiKey,
+		'Content-Type: application/json'
+	);
 
 	$ch = curl_init();
 	curl_setopt( $ch, CURLOPT_URL, $url );
@@ -30,13 +44,7 @@ function sendGoogleCloudMessage( $data, $ids )
 }
 
 
-$app->get('/sendtest', function () {
-
-	$host = 'localhost';
-	$dbname = 'stetro_pickmeapp';
-	$user = 'stetro';
-	$pass = '';	
-	$DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+$app->get('/sendtest', function () use($DBH) {
 	$data = array( 'message' => 'Hello World!' );
 	$STH = $DBH->query('SELECT * from regids');
 	$STH->setFetchMode(PDO::FETCH_ASSOC);
@@ -45,7 +53,50 @@ $app->get('/sendtest', function () {
 		$ids[] = $row['regid'];
 	}
 	sendGoogleCloudMessage(  $data, $ids );
+	echo json_encode($ids);
 });
+
+$app->get('/getnotification',function() use($app, $DBH){
+	$STH = $DBH->prepare('SELECT * from notifications');
+	$STH->execute();
+	echo json_encode($STH->fetchAll(PDO::FETCH_ASSOC));
+});
+$app->get('/notification/', function() use ($app, $DBH){
+
+});
+
+$app->get('/registrationid/', function() use($app, $DBH){
+	$STH = $DBH->prepare('SELECT * from regids');
+	$STH->execute();
+	$STH->setFetchMode(PDO::FETCH_ASSOC);
+	echo json_encode($STH->fetchAll(PDO::FETCH_ASSOC));
+});
+
+$app->post('/registrationid/', function() use ($app, $DBH){
+	$regid = $app->request->post('regid');
+	$phonenumber = $app->request->post('phonenumber');
+
+	$STH = $DBH->prepare('SELECT * from regids where regid = ?');
+	$STH->bindValue(1, $regid,  PDO::PARAM_STR);
+	$STH->execute();
+	$STH->setFetchMode(PDO::FETCH_ASSOC);
+	$result = $STH->fetch();
+	if(empty($result)){
+		$STH = $DBH->prepare('INSERT INTO regids (regid, phone) values (?, ?)');
+		$STH->bindValue(1, $regid,  PDO::PARAM_STR);
+		$STH->bindValue(2, $phonenumber,  PDO::PARAM_STR);
+		$STH->execute();
+		$STH = $DBH->prepare('SELECT * from regids where regid = ?');
+		$STH->bindValue(1, $regid,  PDO::PARAM_STR);
+		$STH->execute();
+		$STH->setFetchMode(PDO::FETCH_ASSOC);
+		$result = $STH->fetch();
+		echo '{"id":"'.$result['id'].'", "regid":"'.$result['regid'].'", "phonenumber":"'.$result['phone'].'"}';
+	}else{
+		echo '{"id":"'.$result['id'].'", "regid":"'.$result['regid'].'", "phonenumber":"'.$result['phone'].'"}';
+	}
+});
+
 $app->get('/',function(){
 	echo "Server is running ...";
 });
