983cd2f8f041e29ae449f10ac3a38dfe6c9eab1e
Steffen Tröster <stetro.freaky@googlemail.com>
2014-05-24 15:21:26 +0200

    adding database support and push with rest api

--- server/index.php
+++ server/index.php
@@ -1,3 +1,55 @@
 <?php
 require 'vendor/autoload.php';
+$app = new \Slim\Slim();
+function sendGoogleCloudMessage( $data, $ids )
+{
+	$url = 'https://android.googleapis.com/gcm/send';
+	$apiKey = 'AIzaSyBcG_V2Aen_fw03WlMZqAo_SgFCpHIwd6U';
+	$post = array(
+	    'registration_ids'  => $ids,
+	        'data'              => $data,
+	    );
+	$headers = array( 
+       'Authorization: key=' . $apiKey,
+        'Content-Type: application/json'
+    );
+
+	$ch = curl_init();
+	curl_setopt( $ch, CURLOPT_URL, $url );
+	curl_setopt( $ch, CURLOPT_POST, true );
+	curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers );
+	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+	curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode( $post ) );
+	$result = curl_exec( $ch );
+	if ( curl_errno( $ch ) )
+	{
+		echo 'GCM error: ' . curl_error( $ch );
+	}
+	curl_close( $ch );
+	echo $result;
+}
+
+
+$app->get('/sendtest', function () {
+
+	$host = 'localhost';
+	$dbname = 'stetro_pickmeapp';
+	$user = 'stetro';
+	$pass = '';	
+	$DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+	$data = array( 'message' => 'Hello World!' );
+	$STH = $DBH->query('SELECT * from regids');
+	$STH->setFetchMode(PDO::FETCH_ASSOC);
+	$ids = array( );
+	while($row = $STH->fetch()) {
+		$ids[] = $row['regid'];
+	}
+	sendGoogleCloudMessage(  $data, $ids );
+});
+$app->get('/',function(){
+	echo "Server is running ...";
+});
+
+$app->run();
+
 ?>
